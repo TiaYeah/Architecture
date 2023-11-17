617a3306a2acbb0215e9fe1828cb8f39fc3cfc17
TiaYeah <nikita58316@mail.ru>
2023-11-17 22:51:31 +0300

    Add files

--- Client/AppClient.cpp
+++ Client/AppClient.cpp
@@ -15,84 +15,50 @@
 
 bool Client::send(const std::string& url, const std::string& msg)
 {
-    /*SocketClient s;
+    SocketClient s;
     int start = clock();
 
     while ((clock() - start) / CLOCKS_PER_SEC < 60) {
-        printf("Time passed %d\n", (clock() - start) / CLOCKS_PER_SEC);
+        
         if (!s.init() || !s.connect(url)) {
             continue;
         }
-        printf("sending text message \"%s\"\n", msg.c_str());
-        int len = s.sendStr(msg);
-        printf("sent %d bytes\n", len);
-        if (len > 0) {
-            return true;
+        std::string filepath;
+        int len;
+
+        if (fileExists(msg)) {
+
+            std::string fileExtension = msg.substr(msg.find_last_of('.'));
+            std::string fileName = msg.substr(msg.find_last_of('\\') + 1, msg.find_last_of('.') - msg.find_last_of('\\') - 1);
+
+            filepath = msg;
+
+
+            std::ifstream f(filepath, std::ios::binary);
+            f.seekg(0, std::ios::end);
+            size_t size = f.tellg();
+            std::string stringFile(size, ' ');
+            f.seekg(0);
+            f.read(&stringFile[0], size);
+
+
+            std::string res = "FILE " + fileName + fileExtension + " " + stringFile;
+
+
+            len = s.sendStr(res);
+            f.close();
         }
+        else {
+            printf("sending text message \"%s\"\n", msg.c_str());
+            len = s.sendStr("TEXT " + msg);
+        }
+        printf("sent %d bytes\n", len);
+
+
+        return len > 0;
     }
     printf("Time is out");
-    return false;*/
-    SocketClient s;
-    if(!s.init() || !s.connect(url))
-        return false;
-
-
-    std::string filename;
-    int len;
-
-    if (fileExists(msg)) {
-        filename = msg; 
-        std::ifstream file;
-        file.open(msg, std::ios::binary); //d:\\18.jpg
-        int file_size2 = std::filesystem::file_size(msg) + 1;
-        /*file.seekg(0, std::ios::end);
-        int size = file.tellg();
-        std::cout << size << std::endl;
-        file.seekg(0, std::ios::beg);*/
-
-        printf("Create buff");
-        char* buff = new char[file_size2 + 5];
-        printf("Reading");
-
-        
-        char ch;
-        int i = 5;
-        file.read(buff, file_size2);
-        for (int i = 5; i < file_size2; i++) {
-            buff[i] = buff[i - 5];
-        }
-        buff[0] = 'F';
-        buff[1] = 'I';
-        buff[2] = 'L';
-        buff[3] = 'E';
-        buff[4] = ' ';
-
-        /*while (!file.eof()) {
-            ch = file.get();
-            buff[i++] = ch;
-        }*/
-        len = s.send(reinterpret_cast<const char*>(buff), file_size2 + 5);
-        file.close();
-        delete[] buff;
-    }
-    else {
-        printf("sending text message \"%s\"\n", msg.c_str());
-        len = s.sendStr(msg);
-    }
-    printf("sent %d bytes\n", len);
+    return false;
 
     
-    printf("file was opened\n");
-
-    //file.open("asd.txt", std::ios::binary);
-    
-
-    //file.read((char*)buff, size);
-    printf("Reading");
-
-
-
-    
-
-    return len > 0;
 }
--- Server/AppServer.cpp
+++ Server/AppServer.cpp
@@ -10,23 +10,23 @@
 
 bool Server::init(int port)
 {
-    if(!m_socket.init(1000) || !m_socket.listen(port))
+    if (!m_socket.init(1000) || !m_socket.listen(port))
         return false;
 
     CreateDirectory("resources", NULL);
 
-    if(!fileWriteExclusive("resources\\CREATED", toStr(m_socket.port()) + "," + toStr(_getpid())))
+    if (!fileWriteExclusive("resources\\CREATED", toStr(m_socket.port()) + "," + toStr(_getpid())))
         return false;
 
     printf("server started: port %d, pid %d\n", m_socket.port(), _getpid());
 
     char* state = fileReadStr("resources\\STATE"); // load state from previous run
-    if(state)
+    if (state)
     {
-        for(std::string& line : split(state, "\n"))
-            if(!line.empty())
+        for (std::string& line : split(state, "\n"))
+            if (!line.empty())
                 m_data.push_back(line);
-        delete [] state;
+        delete[] state;
     }
 
     return true;
@@ -34,75 +34,84 @@ bool Server::init(int port)
 
 void Server::run()
 {
-    while(1)
+    while (1)
     {
         fileWriteStr(std::string("resources\\ALIVE") + toStr(_getpid()), ""); // pet the watchdog
         std::shared_ptr<Socket> client = m_socket.accept(); // accept incoming connection
-        if(!client->isValid())
+        if (!client->isValid())
             continue;
 
         int n = client->recv(); // receive data from the connection, if any
         char* data = client->data();
-        /*std::ofstream image("res.jpg", std::ios::out | std::ios::app);
-
-        char ch;
-        for (int i = 0; i < 15000; i++) {
-            ch = data[i];
-            image.put(ch);
-            std::cout << i << std::endl;
-        }
-        image.clear();
-       
-        std::cout << "Out of loop\n";*/
 
         printf("-----RECV-----\n%s\n--------------\n", n > 0 ? data : "Error");
         const std::vector<std::string>& tokens = split(data, " ");
-        if(tokens.size() >= 2 && tokens[0] == "GET") // this is browser's request
-        {
-            // convert URL to file system path, e.g. request to img/1.png resource becomes request to .\img\1.png file in Server's directory tree
-            const std::string& filename = join(split(tokens[1], "/"), "\\");
-            if(filename == "\\")
-            { // main entry point (e.g. http://localhost:12345/)
-                std::string payload = "";
-                for(auto s : m_data)
-                    payload += (s+"<br>"); // collect all the feed and send it back to browser
-                client->sendStr("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: " + toStr(payload.length()) + "\r\n\r\n" + payload);
-            }
-            else
-                client->sendStr("HTTP/1.1 404 Not Found\r\n\r\n");
-        }
-        else if (tokens.size() >= 2 && tokens[0] == "FILE") {
-            std::fstream file;
-            file.open("vssad", std::ios_base::out || std::ios_base::binary);
-            file.write(tokens[1].c_str(), n - 5);
-            file.close();
-            std::cout << "file saved";
-            //fileWrite("test.jpg", tokens[1].c_str(), tokens[1].length());
-            //std::cout << tokens[1];
-            //std::vector<std::string> splitStr = split(tokens[1], "\\");
-            //std::cout << splitStr[splitStr.size()];
-            //std::string name = "/resources/common/" + tokens[1];
-            //std::string path = ".\\resources\\common\\" + tokens[1];
-            //int prefix = 4 + tokens[1].length() + 1;
-            //int size = n - prefix;
-            //int trys = 0;
-            //if (fileExists(path)) {
-            //    while (fileExists(path) && trys < 20) {
-            //        trys++;
-            //    }
-            //    path = ".\\resources\\common\\(" + toStr(trys) + ")" + tokens[1];
-            //}
-            //// Use data, not tokens, because png hava char terminate characters
-            //// That will cause to not store all file data
-            //fileWrite(path, data + prefix, size);
+        if (tokens.size() >= 2) {// this is browser's request 
+            if (tokens[0] == "GET")
+            {
+                // convert URL to file system path, e.g. request to img/1.png resource becomes request to .\img\1.png file in Server's directory tree
+                const std::string& filename = join(split(tokens[1], "/"), "\\");
+                std::string fileExtension = filename.substr(filename.find_last_of('.') + 1);
 
-            //m_data.push_back(name);
-            //fileAppend("resources\\STATE", m_data.back() + "\n");
-        }
-        else if(n > 0) // this is Client's request who wants to upload some data
-        {
-            m_data.push_back(data); // store it in the feed
-            fileAppend("resources\\STATE", m_data.back() + "\n"); // store it in the file for subsequent runs
+                if (filename == "\\")
+                { // main entry point (e.g. http://localhost:12345/)
+                    std::string payload = "";
+                    for (auto s : m_data) {
+                        std::string fileExtension = s.substr(s.find_last_of('.') + 1);
+                        if (fileExists("resources/" + s) && (fileExtension == "jpg" || fileExtension == "png")) {
+                            payload += "<p><img src=\"/" + s + "\"></p>\n";
+                        }
+                        else {
+                            payload += (s + "<br>"); // collect all the feed and send it back to browser
+                        }
+                    }
+                    client->sendStr("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: " + toStr(payload.length()) + "\r\n\r\n" + payload);
+                }
+                else if (fileExists("resources/" + filename) && (fileExtension == "jpg" || fileExtension == "png"))
+                {
+                    std::ifstream file("resources" + filename, std::ios::binary);
+                    if (file.is_open())
+                    {
+                        file.seekg(0, std::ios::end);
+                        size_t size = file.tellg();
+                        std::string stringFile(size, ' ');
+                        file.seekg(0);
+                        file.read(&stringFile[0], size);
+                        client->sendStr("HTTP/1.1 200 OK\r\nContent-Type: image/png\r\nContent-Length: " + std::to_string(size) + "\r\n\r\n" + stringFile);
+                    }
+                    else
+                    {
+                        std::cout << "Can't open file\n";
+                    }
+                }
+                else
+                {
+                    client->sendStr("HTTP/1.1 404 Not Found\r\n\r\n");
+                }
+            }
+            else if (tokens[0] == "FILE") {
+                std::string filename = tokens[1];
+                std::ofstream file("resources/" + filename, std::ios_base::binary);
+                if (file.is_open()) {
+                    int gap = tokens[0].length() + tokens[1].length() + 2;
+                    int dataSize = n - gap;
+
+                    file.write(&data[gap], dataSize);
+                    file.flush();
+                }
+                else {
+                    std::cout << "Can't open file\n";
+                }
+                file.close();
+
+                m_data.push_back(filename); // store it in the feed
+                fileAppend("resources\\STATE", m_data.back() + "\n"); // store it in the file for subsequent runs
+            }
+            else if (tokens[0] == "TEXT") // this is Client's request who wants to upload some data
+            {
+                m_data.push_back(tokens[1]); // store it in the feed
+                fileAppend("resources\\STATE", m_data.back() + "\n"); // store it in the file for subsequent runs
+            }
         }
     }
 }
